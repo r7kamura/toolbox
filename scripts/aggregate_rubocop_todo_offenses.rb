f9bb8c23dcd3555322d97c1642e6468fb903117f
Ryo Nakamura <r7kamura@gmail.com>
2022-07-26 08:31:39 +0900

    Prefer %H (full commit hash) to %h (abbreviated commit hash)

--- scripts/aggregate_rubocop_todo_offenses.rb
+++ scripts/aggregate_rubocop_todo_offenses.rb
@@ -13,7 +13,7 @@ require 'date'
 days_to_count = 365
 
 days_to_count.times do |i|
-  commit_sha = `git log -1 --format='%h' --before=#{i}.day`.rstrip
+  commit_sha = `git log -1 --format='%H' --before=#{i}.day`.rstrip
   rubocop_todo_content = `git show #{commit_sha}:.rubocop_todo.yml`.rstrip
   offenses_count = rubocop_todo_content.scan(/count: (\d+)/).flatten.map(&:to_i).sum
   date = Date.today - i
