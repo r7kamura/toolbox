5949b61f58237901614e31c38b6f86e45be1ec73
Ryo Nakamura <r7kamura@gmail.com>
2022-07-26 08:20:10 +0900

    Add scripts/aggregate_rubocop_todo_offenses.rb

---
+++ scripts/aggregate_rubocop_todo_offenses.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Aggregate total offenses count in .rubocop_todo.yml by day in TSV format.
+#
+# Usage:
+#
+# ```
+# ruby scripts/aggregate_rubocop_todo_offenses.rb
+# ```
+
+require 'date'
+
+days_to_count = 365
+
+days_to_count.times do |i|
+  commit_sha = `git log -1 --format='%h' --before=#{i}.day`.rstrip
+  rubocop_todo_content = `git show #{commit_sha}:.rubocop_todo.yml`.rstrip
+  offenses_count = rubocop_todo_content.scan(/count: (\d+)/).flatten.map(&:to_i).sum
+  date = Date.today - i
+  puts [
+    date,
+    offenses_count
+  ].join("\t")
+end
